febe045713f2460db29fdd445d50ac6d4bd84db3
JiJogbf <alex.gas1995@ya.ru>
2020-12-13 15:22:14 +0300

    fixing types in types.hpp header

--- Core/src/cpp/main/gas/types.hpp
+++ Core/src/cpp/main/gas/types.hpp
@@ -9,10 +9,10 @@ namespace gas{
     using char_p = char_t*;
     using char_cp = const char_t*;
     
-    using u8 = uint8_t;    
-    using u8 = uint8_t;    
-    using u8 = uint8_t;    
-    using u8 = uint8_t;
+    using u8  = uint8_t;    
+    using u16 = uint16_t;    
+    using u32 = uint32_t;    
+    using u64 = uint64_t;
 }
 
 #endif
